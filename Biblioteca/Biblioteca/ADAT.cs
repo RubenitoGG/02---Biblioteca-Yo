6346807fb22b336235c6fb5447091a94e34e3371
RubenitoGG <ruben.gago.gonzalez@outlook.com>
2018-12-18 13:11:23 +0100

    Initial commit

---
+++ Biblioteca/Biblioteca/ADAT.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Biblioteca
+{
+    public class ADAT
+    {
+        // Invocamos la base de datos:
+        public static bibliotecaDataSet dataSet = new bibliotecaDataSet();
+        public static int registrosActualizados;
+
+        // String para coger la conexión:
+        string conexion = Properties.Settings.Default.bibliotecaConnectionString.ToString();
+
+        // Cogemos datos de la base de datos:
+        public static bibliotecaDataSet.LibroRow libroRow;
+        public static bibliotecaDataSet.ejemplarRow ejemplarRow;
+
+        public void InsertarLibro(string codigo, string isbn, string titulo, string editorial, string descripcion)
+        {
+            // Buscamos el libro para no añadir repetidos:
+            BuscarLibro("isbn", isbn);
+
+            if (libroRow == null)
+            {
+                try
+                {
+                    // Establecemos conexión:
+                    SqlConnection connection = new SqlConnection(this.conexion);
+                    connection.Open();
+                    Debug.WriteLine("Conexión abierta");
+
+                    // Creamos el constructor para insertar:
+                    StringBuilder str_insert = new StringBuilder();
+                    str_insert.AppendFormat("Insert Libro values('{0}', '{1}', '{2}', '{3}', '{4}'" +
+                        ")", codigo, isbn, titulo, editorial, descripcion);
+
+                    // Creamos Query:
+                    SqlCommand comando = new SqlCommand(str_insert.ToString(), connection);
+
+                    // Ejecutar Query:
+                    registrosActualizados = comando.ExecuteNonQuery();
+
+                    // Cerramos la conexión:
+                    connection.Close();
+                    Debug.WriteLine("Conexión cerrada");
+                }
+                catch (Exception er)
+                {
+                    MessageBox.Show("Ha ocurrido un error.", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Debug.WriteLine(er.ToString());
+                }
+            }
+        }
+
+        public void InsertarEjemplar(string codigo, DateTime fecha, int numEjemplar)
+        {
+            BuscarLibro("codigo", codigo);
+
+            try
+            {
+                // Establecemos conexión:
+                SqlConnection connection = new SqlConnection(this.conexion);
+                connection.Open();
+                Debug.WriteLine("Conexión abierta");
+
+                // Creamos el constructor para insertar:
+                StringBuilder str_insert = new StringBuilder();
+                str_insert.AppendFormat("Insert ejemplar values('{0}','{1}','{2}','{3}')", codigo, numEjemplar, fecha, "D");
+
+                // Creamos Query:
+                SqlCommand comando = new SqlCommand(str_insert.ToString(), connection);
+
+                // Ejecutar Query:
+                registrosActualizados = comando.ExecuteNonQuery();
+
+                // Cerramos la conexión:
+                connection.Close();
+                Debug.WriteLine("Conexión cerrada");
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Ha ocurrido un error.", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                Debug.WriteLine(er.ToString());
+            }
+        }
+
+        public void BuscarEjemplar(string codigo)
+        {
+            // Limpiamos la referencia de ejemplar:
+            dataSet.ejemplar.Clear();
+
+            try
+            {
+                // Establecemos conexión:
+                SqlConnection connection = new SqlConnection(this.conexion);
+                connection.Open();
+                Debug.WriteLine("Conexión abierta");
+
+                // Creamos el constructor para insertar:
+                StringBuilder str_insert = new StringBuilder();
+                str_insert.AppendFormat("Select * from ejemplar Where codigo = " + codigo);
+
+                // Creamos Query:
+                SqlDataAdapter adapter = new SqlDataAdapter(str_insert.ToString(), connection);
+
+                // Ejecutamos Query:
+                adapter.Fill(dataSet.ejemplar);
+                Debug.WriteLine(dataSet.ejemplar);
+
+                // Cerramos conexión:
+                connection.Close();
+                Debug.WriteLine("Conexión cerrada");
+
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Ha ocurrido un error.", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                Debug.WriteLine(er.ToString());
+            }
+
+        }
+
+        public void BuscarLibro(string campo, string valor)
+        {
+            // Limpiamos la referencia a libro:
+            dataSet.Libro.Clear();
+
+            try
+            {
+                // Establecemos conexión:
+                SqlConnection connection = new SqlConnection(this.conexion);
+                connection.Open();
+                Debug.WriteLine("Conexión abierta");
+
+                // Creamos el constructor para insertar:
+                StringBuilder str_insert = new StringBuilder();
+                str_insert.AppendFormat("Select * from Libro Where " + campo + " = " + valor);
+
+                // Creamos Query:
+                SqlDataAdapter adapter = new SqlDataAdapter(str_insert.ToString(), connection);
+
+                // Ejecutamos Query:
+                adapter.Fill(dataSet.Libro);
+
+                // Guardamos 'libroRow' si encontramos el libro:
+                if (dataSet.Libro.Count == 1)
+                {
+                    libroRow = dataSet.Libro[0];
+                    Debug.WriteLine("Libro guardado");
+                }
+                else
+                {
+                    if (dataSet.Libro.Count == 0)
+                        libroRow = null;
+                }
+
+                // Cerramos la conexión:
+                connection.Close();
+                Debug.WriteLine("Conexión cerrada");
+            }
+            catch (Exception er)
+            {
+                libroRow = null;
+                MessageBox.Show("Ha ocurrido un error.", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                Debug.WriteLine(er.ToString());
+            }
+        }
+
+        public void ModificarLibro(string isbn, string titulo, string editorial, string descripcion)
+        {
+            try
+            {
+                if (libroRow != null)
+                {
+
+
+                    // Establecemos conexión:
+                    SqlConnection connection = new SqlConnection(this.conexion);
+                    connection.Open();
+                    Debug.WriteLine("Conexión abierta");
+
+                    // Creamos el constructor:
+                    StringBuilder str_insert = new StringBuilder();
+                    str_insert.AppendFormat("Update Libro set isbn='{0}',titulo='{1}',editorial='{2}',descripcion='{3}' Where codigo='{4}'", isbn, titulo, editorial, descripcion, libroRow.codigo);
+
+                    // Creamos Query:
+                    SqlCommand comando = new SqlCommand(str_insert.ToString(), connection);
+
+                    // Ejecutar Query:
+                    registrosActualizados = comando.ExecuteNonQuery();
+
+                    // Cerramos la conexión:
+                    connection.Close();
+                    Debug.WriteLine("Conexión cerrada");
+                }
+                else
+                    MessageBox.Show("Busque un libro", "", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Ha ocurrido un error.", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                Debug.WriteLine(er.ToString());
+            }
+        }
+
+        public void EliminarLibro()
+        {
+            try
+            {
+                if(libroRow != null)
+                {
+                    // Establecemos conexión:
+                    SqlConnection connection = new SqlConnection(this.conexion);
+                    connection.Open();
+                    Debug.WriteLine("Conexión abierta");
+
+                    // Creamos el constructor:
+                    StringBuilder str_insert = new StringBuilder();
+                    str_insert.AppendFormat("Delete from ejemplar Where codigo = " + libroRow.codigo);
+                    str_insert.AppendFormat("Delete from Libro Where codigo = " + libroRow.codigo);
+
+                    // Creamos Query:
+                    SqlCommand comando = new SqlCommand(str_insert.ToString(), connection);
+
+                    // Ejecutamos Query:
+                    registrosActualizados = comando.ExecuteNonQuery();
+
+                    // Cerramos la conexión:
+                    connection.Close();
+                    Debug.WriteLine("Conexión cerrada");
+
+                    if (registrosActualizados > 0)
+                        libroRow = null;
+
+                    MessageBox.Show("Libro eliminado", "", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Ha ocurrido un error.", "", MessageBoxButton.OK, MessageBoxImage.Error);
+                Debug.WriteLine(er.ToString());
+            }
+        }
+    }
+}
---
+++ Biblioteca/Biblioteca/VentanaLibro.xaml.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Biblioteca
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaLibro.xaml
+    /// </summary>
+    public partial class VentanaLibro : Window
+    {
+        // Clase que conecta a la base de datos:
+        ADAT adat = new ADAT();
+
+        public VentanaLibro()
+        {
+            InitializeComponent();
+            rellenarCombo();
+        }
+
+        private void rellenarCombo()
+        {
+            for (int i = 0; i < ADAT.dataSet.Libro.DefaultView.Table.Columns.Count; i++)
+                cbCampos.Items.Add(ADAT.dataSet.Libro.DefaultView.Table.Columns[i].ColumnName);
+        }
+
+        private void btn_registrar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txt_codigo.Text) && !String.IsNullOrEmpty(txt_isbn.Text) && !String.IsNullOrEmpty(txt_titulo.Text)
+                && !String.IsNullOrEmpty(txt_editorial.Text) && !String.IsNullOrEmpty(txt_descripcion.Text))
+            {
+                adat.InsertarLibro(txt_codigo.Text, txt_isbn.Text, txt_titulo.Text, txt_editorial.Text, txt_descripcion.Text);
+
+                mostrarEjemplares();
+                limpiarCampos();
+            }
+            else
+                MessageBox.Show("Faltan campos por cubrir", "", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+
+        private void limpiarCampos()
+        {
+            txt_codigo.Text = "";
+            txt_isbn.Text = "";
+            txt_titulo.Text = "";
+            txt_editorial.Text = "";
+            txt_descripcion.Text = "";
+
+            txt_codigo_buscar.Text = "";
+            txt_numEjemplar.Text = "";
+            dp_FechaPublicacion.Text = "";
+
+            cbCampos.SelectedItem = null;
+            tbBusqueda.Text = "";
+
+            dgEjemplares.ItemsSource = "";
+        }
+
+        private void btn_GuardarEjemplar_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(txt_codigo_buscar.Text) && !String.IsNullOrEmpty(dp_FechaPublicacion.Text) && !String.IsNullOrEmpty(txt_numEjemplar.Text))
+            {
+                adat.InsertarEjemplar(txt_codigo_buscar.Text, Convert.ToDateTime(dp_FechaPublicacion.Text), Convert.ToInt32(txt_numEjemplar.Text));
+                mostrarEjemplares();
+
+                txt_codigo_buscar.Text = "";
+                dp_FechaPublicacion.Text = "";
+                txt_numEjemplar.Text = "";
+
+                mostrarLibro();
+            }
+            else
+                MessageBox.Show("Faltan campos por cubrir", "", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+
+        private void mostrarEjemplares()
+        {
+            dgEjemplares.ItemsSource = "";
+            if (ADAT.libroRow != null)
+            {
+                adat.BuscarEjemplar(ADAT.libroRow.codigo);
+                dgEjemplares.ItemsSource = ADAT.dataSet.ejemplar;
+            }
+        }
+
+        private void btn_buscarLibro_Click(object sender, RoutedEventArgs e)
+        {
+            if (!String.IsNullOrEmpty(tbBusqueda.Text) && cbCampos.SelectedItem != null)
+            {
+                adat.BuscarLibro(cbCampos.SelectedItem.ToString(), tbBusqueda.Text);
+
+                if (ADAT.libroRow != null)
+                {
+                    mostrarLibro();
+                    mostrarEjemplares();
+                }
+                else
+                    MessageBox.Show("Libro no encontrado", "", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+                MessageBox.Show("Faltan campos por cubrir", "", MessageBoxButton.OK, MessageBoxImage.Error);
+
+        }
+
+        private void mostrarLibro()
+        {
+            txt_codigo.Text = ADAT.libroRow.codigo.ToString();
+            txt_isbn.Text = ADAT.libroRow.isbn.ToString();
+            txt_titulo.Text = ADAT.libroRow.titulo.ToString();
+            txt_editorial.Text = ADAT.libroRow.editorial.ToString();
+            txt_descripcion.Text = ADAT.libroRow.descripcion.ToString();
+        }
+
+        private void btNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            limpiarCampos();
+            dgEjemplares.ItemsSource = "";
+        }
+
+        private void btn_eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            adat.EliminarLibro();
+            limpiarCampos();
+        }
+
+        private void btn_modificar_Click(object sender, RoutedEventArgs e)
+        {
+            if(!String.IsNullOrEmpty(txt_isbn.Text) && !String.IsNullOrEmpty(txt_titulo.Text)
+                && !String.IsNullOrEmpty(txt_editorial.Text) && !String.IsNullOrEmpty(txt_descripcion.Text))
+            adat.ModificarLibro(txt_isbn.Text, txt_titulo.Text, txt_editorial.Text, txt_descripcion.Text);
+
+            limpiarCampos();
+        }
+    }
+}
